7d957ac6507d4288b8d2a5d0e969ab2923048735
unknown <Leisha Penny@.(none)>
2009-02-25 13:27:15 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Health Now!</title>
+        <!-- Author: Leisha Penny
+        // Web Development MWF-->
+        <link href="styles/default.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div id="banner">
+        <img src="images/logoHnow.gif" align="left" alt="Health Now logo" />
+        <h2>Students in Missippi Get Healthy NOW!</h2>
+        </div> <!-- end banner div-->
+        
+        <div id="container">
+            <div id="lcol">
+            <p>Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy
+            text ever since the 1500s,
+            when an unknown printer took a galley of type and
+            </p>
+            </div> <!-- end lcol div-->
+            <div id="content">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.
+            </p>
+            </div><!--end content div-->
+            <div id="rcol">
+            <p>Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry.
+            Lorem Ipsum has been the industry's standard
+            dummy text ever since the 1500s,
+            when an unknown printer took a galley 
+            </p>
+            </div> <!--end rcol div-->
+            <!--<p style="clear: both;">This clear is important when floating
+            elements in a div container. The tells the element to clear all 
+            other elements, or you can use clear:left or both in CSS</p>-->
+        </div><!--end of container div</p>-->
+            
+        <div id="footer">
+        <p>Author: Leisha Penny</p>
+        <p><a href="mailto:leihsap@gmail.com">Contact Me</a></p>
+        </div><!--end footer div-->
+        
+    </body>
+</html>
